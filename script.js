a255fac8ba4d9d3d0a8896f5aa80553c20aaa811
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-03-02 14:22:54 +0200

    Add reset Grid button and functionality

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const grid = document.querySelector(".grid");
 for (let i=0; i<256; i++) {
     const currentDiv = document.createElement("div");
+    // currentDiv.textContent = `${i}`;
+    // currentDiv.height = 10;
     grid.appendChild(currentDiv);
 }
 
 const gridList = grid.childNodes
 
-gridList.forEach(box => box.addEventListener("mouseover", () =>
+gridList.forEach(box => box.addEventListener("mousedown", () =>
 box.classList.add("colored")));
+
+function resetGrid() {
+    gridList.forEach(box => box.classList.remove("colored"))
+};
+
+const resetButton = document.querySelector("#reset")
+resetButton.addEventListener("click", () => resetGrid())
